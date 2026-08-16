57b6e1bb42c62ee50540bd237134188688d38ca3
Mr Stonkles <voby@waifu.club>
2026-08-16 17:08:36 +0600

    snipe into moderation folder

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -3,5 +3,7 @@ from base.Xrypton import Bot
 
 async def setup(bot: Bot) -> None:
     from .moderation import Moderation
+    from .snipe import Snipe
 
+    await bot.add_cog(Snipe(bot))
     await bot.add_cog(Moderation(bot))
